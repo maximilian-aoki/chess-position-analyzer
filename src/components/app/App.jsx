b775e40160a5fece107e35cd66b2497712367186
Maximilian Aoki <135271252+maximilian-aoki@users.noreply.github.com>
2024-06-26 17:57:56 -0400

    get ui base somewhat workable

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -9,15 +9,21 @@ console.log(boardManager);
 
 export default function App() {
   return (
-    <div className="text-green-700">
-      <header>
-        <h1>Chess Position Analyzer</h1>
+    <div className="flex-1 flex flex-col">
+      <header className="flex-initial h-20 md:h-28 bg-slate-200 p-4 grid content-center">
+        <h1 className="font-bold text-xl md:text-3xl md:px-12">
+          CHESS POSITION ANALYSER
+        </h1>
       </header>
-      <main>
-        <h2>Main Content</h2>
+      <main className="flex-1 bg-slate-300 flex flex-col items-center">
+        <div className="flex-1 w-full md:w-[768px] bg-slate-600">
+          <h2>Main Content</h2>
+        </div>
       </main>
-      <footer>
-        <h2>2024 Copyright Maximilian Aoki</h2>
+      <footer className="flex-initial h-20 bg-slate-400 grid place-content-center">
+        <h2 className="font-bold italic text-lg text-white">
+          created by Maximilian Aoki
+        </h2>
       </footer>
     </div>
   );
